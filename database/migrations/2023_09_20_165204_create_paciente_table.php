db6447d301a8b14d4b2121f18427e00b2b0430b2
giovanyrosales <gioer21@gmail.com>
2023-09-20 11:32:48 -0600

    primeras tablas

---
+++ database/migrations/2023_09_20_165204_create_paciente_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePacienteTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('paciente', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('medico_id')->unsigned();
+            $table->bigInteger('tipo_id')->unsigned();
+            $table->bigInteger('estado_civil_id')->unsigned();
+            $table->bigInteger('tipo_documento_id')->unsigned();
+            $table->bigInteger('profesion_id')->unsigned();
+
+            $table->string('nombres', 150)->nullable();
+            $table->string('apellidos', 150)->nullable();
+            $table->date('fecha_nacimiento');
+            $table->char('sexo')->nullable();
+            $table->string('referido_por', 300)->nullable();
+            $table->string('num_documento', 100);
+            $table->string('correo', 150)->nullable();
+            $table->string('celular', 25)->nullable();
+            $table->string('telefono', 25)->nullable();
+            $table->string('direccion', 550)->nullable();
+            //$table->boolean('estado')->default('0');
+
+            $table->foreign('tipo_documento_id')->references('id')->on('tipo_documento');
+            $table->foreign('medico_id')->references('id')->on('medico');
+            $table->foreign('tipo_id')->references('id')->on('tipo_paciente');
+            $table->foreign('estado_civil_id')->references('id')->on('estado_civil');
+            $table->foreign('profesion_id')->references('id')->on('profesion');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('paciente');
+    }
+}
---
+++ database/migrations/2023_09_20_171202_create_medico_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMedicoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medico', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('usuario_id')->unsigned();
+            $table->string('nombre', 150)->nullable();
+            $table->string('apellido', 150)->nullable();
+            $table->string('telefono', 25)->nullable();
+
+
+            $table->foreign('usuario_id')->references('id')->on('usuario');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('medico');
+    }
+}
