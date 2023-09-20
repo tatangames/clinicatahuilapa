dc7a07c894cb7a9c5e29ac19f4925319122e1d3c
tatangames <tatangamess@gmail.com>
2023-09-20 09:27:37 -0600

    base

--- config/filesystems.php
+++ config/filesystems.php
@@ -53,6 +53,11 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
         ],
 
+        'archivos' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/archivos'),
+        ],
+
     ],
 
     /*
