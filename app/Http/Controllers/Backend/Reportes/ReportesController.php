7fcc82a725d77fb3942bb47269f5bf99677760b1
Tatan <tatangamess@gmail.com>
2025-10-31 14:49:05 -0600

    lunes cambio2

--- app/Http/Controllers/Backend/Reportes/ReportesController.php
+++ app/Http/Controllers/Backend/Reportes/ReportesController.php
@@ -1451,8 +1451,8 @@ class ReportesController extends Controller
 
 
 
-        $mpdf = new \Mpdf\Mpdf(['format' => 'LETTER']);
-        //$mpdf = new \Mpdf\Mpdf(['tempDir' => sys_get_temp_dir(), 'format' => 'LETTER']);
+        //$mpdf = new \Mpdf\Mpdf(['format' => 'LETTER']);
+        $mpdf = new \Mpdf\Mpdf(['tempDir' => sys_get_temp_dir(), 'format' => 'LETTER']);
 
 
 
@@ -1595,8 +1595,8 @@ class ReportesController extends Controller
         }
 
         // ---------- Instanciar mPDF ----------
-        $mpdf = new \Mpdf\Mpdf(['format' => 'LETTER']);
-        //$mpdf = new \Mpdf\Mpdf(['tempDir' => sys_get_temp_dir(), 'format' => 'LETTER']);
+        //$mpdf = new \Mpdf\Mpdf(['format' => 'LETTER']);
+        $mpdf = new \Mpdf\Mpdf(['tempDir' => sys_get_temp_dir(), 'format' => 'LETTER']);
 
 
 
